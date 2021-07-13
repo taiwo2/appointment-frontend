bd179491ae4d288440e440b3c49356d0ec43eab0
taiwo2 <adetonat@ymail.com>
2021-07-13 12:08:43 +0100

    Update index.js file with store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Provider as AlertProvider } from 'react-alert';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <AlertProvider template={AlertTemplate}>
+        <App />
+      </AlertProvider>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
