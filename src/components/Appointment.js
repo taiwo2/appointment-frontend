03ac53d57ecea47b10c75a0f4b982015ab031530
taiwo2 <adetonat@ymail.com>
2021-07-13 12:52:53 +0100

    State management for appointment.js content

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -1,6 +1,57 @@
 import React from 'react'
 
-const Appointment = () =>{
+const Appointment = () => {
+  const [content, setContent] = useState('');
+  const [doctor, setDoctor] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [successful, setSuccessful] = useState(false);
+  const [error, setError] = useState(false);
+  const { user: currentUser } = useSelector(state => state.auth);
+  const alert = useAlert();
+  if (!currentUser) {
+    return <Redirect to="/login" />;
+  }
+
+  const { id } = useParams();
+  useEffect(() => {
+    UserService.getAppointment(currentUser.user.id, id).then(
+      response => {
+        setContent(response.data);
+        return response.data.doctor_id;
+      },
+      error => {
+        setLoading(false);
+        setError(true);
+        const message = (error.response
+            && error.response.data
+            && error.response.data.message)
+          || error.message
+          || error.toString();
+
+        setContent(message);
+      },
+    ).then(doctorId => UserService.getDoctor(doctorId))
+      .then(response => {
+        setLoading(false);
+        setDoctor(response.data);
+      });
+  }, []);
+
+  const handleClick = () => {
+    setLoading(true);
+    UserService.deleteAppointment(currentUser.user.id, id).then(() => {
+      alert.show('Appointment Deleted', {
+        type: 'success',
+        timeout: 5000,
+      });
+      setLoading(false);
+      setSuccessful(true);
+    });
+  };
+
+  if (successful) {
+    return <Redirect to="/appointments" />;
+  }
   return (
     <div className="container">
        <header className="jumbotron">
