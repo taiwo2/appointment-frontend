b0fb6e3bbb0a863e9ed8ed0a36e0cc676c948b60
taiwo2 <adetonat@ymail.com>
2021-07-13 12:34:52 +0100

    Add indes,js to reducer

---
+++ src/components/Register.js
@@ -0,0 +1,77 @@
+import React from 'react'
+
+const Register = () =>{
+  return (
+    <div className="col-md-12">
+      <div className="card card-container">
+        <img
+          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+          alt="profile-img"
+          className="profile-img-card"
+        />
+
+        <Form onSubmit={handleRegister} ref={form}>
+          {!successful && (
+            <div>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  name="name"
+                  value={name}
+                  onChange={onChangeName}
+                  validations={[required, vname]}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <Input
+                  type="text"
+                  className="form-control"
+                  name="email"
+                  value={email}
+                  onChange={onChangeEmail}
+                  validations={[required, validEmail]}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <Input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  value={password}
+                  onChange={onChangePassword}
+                  validations={[required, vpassword]}
+                />
+              </div>
+
+              <div className="form-group">
+                <button className="btn btn-primary btn-block" disabled={loading} type="submit">
+                  {loading && (
+                  <span className="spinner-border spinner-border-sm" />
+                  )}
+                  <span>Sign Up</span>
+                </button>
+              </div>
+            </div>
+          )}
+
+          {message && (
+            <div className="form-group">
+              <div className={successful ? 'alert alert-success' : 'alert alert-danger'} role="alert">
+                {message}
+              </div>
+            </div>
+          )}
+          <CheckButton style={{ display: 'none' }} ref={checkBtn} />
+        </Form>
+      </div>
+    </div>
+  )
+}
+
+export default Register
