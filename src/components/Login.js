3b5a2b6b9570c7e0deb78a563a5758f2f8b2424f
taiwo2 <adetonat@ymail.com>
2021-07-13 13:02:19 +0100

    Add to updateredux to Home.js page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,76 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { useAlert } from 'react-alert';
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
+import CheckButton from 'react-validation/build/button';
+
+import { login } from '../actions/auth';
+
+const required = value => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+  return '';
+};
 
 const Login = () =>{
+  const form = useRef();
+  const checkBtn = useRef();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const { isLoggedIn } = useSelector(state => state.auth);
+  const { message } = useSelector(state => state.message);
+  const alert = useAlert();
+  const dispatch = useDispatch();
+
+  const onChangeEmail = e => {
+    const email = e.target.value;
+    setEmail(email);
+  };
+
+  const onChangePassword = e => {
+    const password = e.target.value;
+    setPassword(password);
+  };
+
+  const handleLogin = e => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    form.current.validateAll();
+
+    // eslint-disable-next-line no-underscore-dangle
+    if (checkBtn.current.context._errors.length === 0) {
+      dispatch(login(email, password))
+        .then(() => {
+          setLoading(false);
+          alert.show('You are logged in', {
+            type: 'success',
+            timeout: 5000,
+          });
+        })
+        .catch(() => {
+          setLoading(false);
+        });
+    } else {
+      setLoading(false);
+    }
+  };
+
+  if (isLoggedIn) {
+    return <Redirect to="/doctors" />;
+  }
+
   return (
     <div className="col-md-12">
       <div className="card card-container">
