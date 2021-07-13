204dfadb4e721b0bce2c6c862028063bf4ebd916
taiwo2 <adetonat@ymail.com>
2021-07-13 13:08:07 +0100

    Add Update state to Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,115 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
+import CheckButton from 'react-validation/build/button';
+import { isEmail } from 'validator';
+import { useAlert } from 'react-alert';
+import { register } from '../actions/auth';
 
-const Register = () =>{
+const required = value => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+  return null;
+};
+
+const validEmail = value => {
+  if (!isEmail(value)) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This is not a valid email.
+      </div>
+    );
+  }
+  return null;
+};
+
+const vname = value => {
+  if (value.length < 3 || value.length > 20) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        The name must be between 3 and 20 characters.
+      </div>
+    );
+  }
+  return null;
+};
+
+const vpassword = value => {
+  if (value.length < 6 || value.length > 40) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        The password must be between 6 and 40 characters.
+      </div>
+    );
+  }
+  return null;
+};
+
+const Register = () => {
+  const form = useRef();
+  const checkBtn = useRef();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [successful, setSuccessful] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { message } = useSelector(state => state.message);
+  const dispatch = useDispatch();
+  const alert = useAlert();
+  const onChangeName = e => {
+    const name = e.target.value;
+    setName(name);
+  };
+
+  const onChangeEmail = e => {
+    const email = e.target.value;
+    setEmail(email);
+  };
+
+  const onChangePassword = e => {
+    const password = e.target.value;
+    setPassword(password);
+  };
+
+  const handleRegister = e => {
+    e.preventDefault();
+    setLoading(true);
+    setSuccessful(false);
+
+    form.current.validateAll();
+
+    // eslint-disable-next-line no-underscore-dangle
+    if (checkBtn.current.context._errors.length === 0) {
+      dispatch(register(name, email, password))
+        .then(() => {
+          setLoading(false);
+          setSuccessful(true);
+          alert.show('Account created', {
+            type: 'success',
+            timeout: 5000,
+          });
+        })
+        .catch(() => {
+          setLoading(false);
+          setSuccessful(false);
+        });
+    } else {
+      setLoading(false);
+    }
+  };
+  if (successful) {
+    return <Redirect to="/doctors" />;
+  }
+
   return (
     <div className="col-md-12">
       <div className="card card-container">
