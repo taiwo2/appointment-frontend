affe84b722f41dfec36f279d8cdbcf6fc72b28b2
taiwo2 <adetonat@ymail.com>
2021-07-13 12:46:39 +0100

    Create App.css file

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Provider as AlertProvider } from 'react-alert';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AlertTemplate from 'react-alert-template-basic';
 import store from './store';
+import './App.css';
 
 ReactDOM.render(
   <Provider store={store}>
